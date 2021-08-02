46d540342e54806cdf1e3a24092e39c8bf7e97f4
Pedro Alex L Martins <piedroalex@gmail.com>
2021-08-01 23:41:45 -0300

    Criando mais componentes.

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosPesquisaComponent {
   lancamentos = [
-    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '10/07/2021', dataPagamento: null, valor: 4.55, pessoa: 'Padaria Canindé' },
-    { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '02/07/2021', dataPagamento: '01/07/2021', valor: 8000, pessoa: 'Atacadão' },
-    { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '05/07/2021', dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda' },
-    { tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: '10/07/2021', dataPagamento: '03/07/2021', valor: 800, pessoa: 'Escola CPI' },
-    { tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: '22/07/2021', dataPagamento: null, valor: 55000, pessoa: 'Antônio da Silva' },
-    { tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '07/07/2021', dataPagamento: '06/07/2021', valor: 1750, pessoa: 'Imobiliária Rocha e Rocha' },
-    { tipo: 'DESPESA', descricao: 'Mensalidade da academia', dataVencimento: '03/07/2021', dataPagamento: null, valor: 180, pessoa: 'Academia Selfit' }
+    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2021, 7, 10), dataPagamento: null, valor: 4.55, pessoa: 'Padaria Canindé' },
+    { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2021, 7, 2), dataPagamento: new Date(2021, 7, 1), valor: 8000, pessoa: 'Atacadão' },
+    { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: new Date(2021, 7, 5), dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda' },
+    { tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: new Date(2021, 7, 10), dataPagamento: new Date(2021, 7, 3), valor: 800, pessoa: 'Escola CPI' },
+    { tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: new Date(2021, 7, 22), dataPagamento: null, valor: 55000, pessoa: 'Antônio da Silva' },
+    { tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: new Date(2021, 7, 7), dataPagamento: new Date(2021, 7, 6), valor: 1750, pessoa: 'Imobiliária Rocha e Rocha' },
+    { tipo: 'DESPESA', descricao: 'Mensalidade da academia', dataVencimento: new Date(2021, 7, 3), dataPagamento: null, valor: 180, pessoa: 'Academia Selfit' }
   ]
 }

37817b99924e6fef0ea8da301ee98c96d1be0c5a
Pedro Alex L Martins <piedroalex@gmail.com>
2021-09-09 11:37:58 -0300

    Fazendo o redirecionamento na raiz do path.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
   { path: 'lancamentos', component: LancamentosPesquisaComponent },
   { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
   { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },

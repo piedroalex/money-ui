f882a150426c95d7ccf2c5dfeeb964d4579051bd
Pedro Alex L Martins <piedroalex@gmail.com>
2021-09-09 11:34:24 -0300

    Implementando navegação imperativa.

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -8,7 +8,7 @@ import { CategoriaService } from './../../categorias/categoria.service';
 import { PessoaService } from './../../pessoas/pessoa.service';
 import { LancamentoService } from './../../lancamentos/lancamento.service';
 import { Lancamento } from './../../core/model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-lancamento-cadastro',
@@ -32,7 +32,8 @@ export class LancamentoCadastroComponent implements OnInit {
     private lancamentoService: LancamentoService,
     private toasty: ToastyService,
     private errorHandler: ErrorHandlerService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -64,10 +65,11 @@ export class LancamentoCadastroComponent implements OnInit {
 
   adicionarLancamento(form: FormControl){
     this.lancamentoService.adicionar(this.lancamento)
-      .then(() => {
+      .then(lancamentoAdicionado => {
         this.toasty.success('Lançamento adicionado com sucesso!');
-        form.reset();
-        this.lancamento = new Lancamento();
+        //form.reset();
+        //this.lancamento = new Lancamento();
+        this.router.navigate(['/lancamentos', lancamentoAdicionado.codigo]);
       })
       .catch(erro => this.errorHandler.handle(erro));
   }
@@ -99,5 +101,15 @@ export class LancamentoCadastroComponent implements OnInit {
       this.pessoas = pessoas.map(p => ({ label: p.nome, value: p.codigo }));
     })
       .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  novo(form: FormControl){
+    form.reset();
+
+    setTimeout(function() {
+      this.lancamento = new Lancamento();
+    }.bind(this), 1);
+
+    this.router.navigate(['/lancamentos/novo']);
   }
 }

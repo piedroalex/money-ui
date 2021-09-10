b48d0ed02e4de80cd944f0c6ca9d1217b116b2a8
Pedro Alex L Martins <piedroalex@gmail.com>
2021-09-10 00:34:09 -0300

    Resolvendo o caso do refresh token expirar.

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,18 +1,29 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
 
 import { ToastyService } from 'ng2-toasty';
 
+import { NotAuthenticatedError } from './../seguranca/money-http';
+
 @Injectable()
 export class ErrorHandlerService {
 
-  constructor(private toasty: ToastyService) { }
+  constructor(
+    private toasty: ToastyService,
+    private router: Router
+  ) { }
 
   handle(errorResponse: any) {
     let msg: string;
 
     if(typeof errorResponse === 'string'){
       msg = errorResponse;
+
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
+
     } else if (errorResponse instanceof Response && errorResponse.status >= 400 && errorResponse.status <= 499) {
       let errors;
       msg = 'Ocorreu um erro ao processar a sua solicitação';

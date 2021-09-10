f772e9682abf173255ed3e725adfbb4c7aa86101
Pedro Alex L Martins <piedroalex@gmail.com>
2021-09-10 01:15:26 -0300

    Fazendo build para o ambiente de produção.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { CoreModule } from './core/core.module';
-
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { SegurancaModule } from './seguranca/seguranca.module';
+import { CoreModule } from './core/core.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
-import { AppRoutingModule } from './app-routing.module';
-
 
 @NgModule({
   declarations: [

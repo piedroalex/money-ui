c2332ac2835038adc509db8e12284a62c5c8e1a1
Pedro Alex L Martins <piedroalex@gmail.com>
2021-09-17 12:50:43 -0300

    Plotando gráficos com dados estáticos.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,18 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PanelModule } from 'primeng/panel';
+import { ChartModule } from 'primeng/chart';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
+
+    PanelModule,
+    ChartModule,
 
     SharedModule,
     DashboardRoutingModule
